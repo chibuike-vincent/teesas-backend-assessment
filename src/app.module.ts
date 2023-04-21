695c482f34a332f9de7a6fa4b18d77ac58d031fc
chibuike-vincent <chibuikepatrick2@gmail.com>
2023-04-21 09:14:15 +0100

    added a procfile for heroku deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { modules } from './customModule';
-
 @Module({
   imports: [TypeOrmModule.forRoot(
     {
       type: "postgres",
-      host: process.env.HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DATABASE,
+      host: "postgresql-122371-0.cloudclusters.net",
+      port: 18821,
+      username: "teesas",
+      password: "password1@",
+      database: "teesas",
       entities: ["dist/**/*.entity{.ts,.js}"],
       synchronize: true
     }
